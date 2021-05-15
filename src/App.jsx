85d73309b6a9144f311e17c33ee3dfcfb8e6717c
huss <hussein.odeh@gmail.com>
2021-05-15 21:06:33 +0200

    feat: App component retrieves movies and series data, storing them in the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,23 @@
-const App = () => <div className='app'></div>;
+import React, { useState, useEffect } from 'react';
+import { getMockedData as getData } from './Services/ApiClient';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [series, setSeries] = useState([]);
+  useEffect(() => {
+    getData().then((data) => {
+      if (data) {
+        setMovies(
+          data.entries.filter((entry) => entry.programType === 'movie')
+        );
+        setSeries(
+          data.entries.filter((entry) => entry.programType === 'series')
+        );
+      }
+    });
+  }, []);
+
+  return <div className='app'></div>;
+};
+
 export default App;
