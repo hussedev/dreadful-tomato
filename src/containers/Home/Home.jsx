af389da28e6c90e93438c1fbf906e621dd07ba5b
huss <hussein.odeh@gmail.com>
2021-05-17 01:19:09 +0200

    feat: created home component

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+import Poster from '../../components/Poster';
+import moviesPoster from '../../assets/movies.png';
+import seriesPoster from '../../assets/series.png';
+import './Home.scss';
 
-const Home = () => <div></div>;
+const Home = () => (
+  <div className='home-container'>
+    <div className='navbar-container'>
+      <Navbar currentPage='/' />
+    </div>
+    <div className='home'>
+      <div className='content-container'>
+        <Poster poster={moviesPoster} title='Movies' link='/movies'></Poster>
+        <Poster poster={seriesPoster} title='Series' link='/series'></Poster>
+      </div>
+      <div className='footer-container'>
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
 
 export default Home;
